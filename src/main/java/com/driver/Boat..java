c6d944df92f97287766da1223d48090f7cccbe3a
Ajay Dattatray <56440284+ajay7175@users.noreply.github.com>
2023-10-28 15:03:22 +0530

    Create Boat..java

---
+++ src/main/java/com/driver/Boat..java
@@ -0,0 +1,20 @@
+package com.driver;
+
+public class Boat implements WaterVehicle  {
+    private int capacity;
+private String name;
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
